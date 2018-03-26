60e0158162649a8d6ace0c7bc2361bce09d8c199
flavio.dipalo <flavio.dipalo@gmail.com>
2018-03-26 17:29:54 +0200

    code refactoring

--- our_code/main_cython.py
+++ our_code/main_cython.py
@@ -3,3 +3,4 @@ from data.movielens_1m.Movielens1MReader import Movielens1MReader
 
 c = CythonEpoch()
 #recommender.fit(URM_train=URM_train, learning_rate=1e-1, lasso_term=5, ridge_term=1e-2 , iterations_in=500 , threshold_in=5 )
+#
