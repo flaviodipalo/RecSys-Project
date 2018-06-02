b951a713b265569da83ea04ba389a70bedabdc0d
alessio <alessio.baccell@mail.polimi.it>
2018-06-02 21:14:20 +0200

    Import changes

--- our_code/main.py
+++ our_code/main.py
@@ -1,5 +1,5 @@
-import sys
-sys.path.append('/usr/local/lib/python3.6/site-packages')
+#import sys
+#sys.path.append('/usr/local/lib/python3.6/site-packages')
 
 from SLIM_RMSE_Cython_Epoch import SLIM_RMSE_Cython_Epoch
 from SLIM_RMSE_Cython import SLIM_RMSE_Cython
@@ -81,9 +81,9 @@ def run_recommender_optimization(normalized=False, popular=False):
     parameterSearch.evaluate_on_test(URM_test)
 
 
-#run_recommender(normalized, popular)
-#from telegram_bot import TelegramBot
-#telegram_bot = TelegramBot(chat_id = '65065237')
-#telegram_bot.send_message('Optimization startded: '+str(normalized)+str(popular))
+run_recommender(normalized, popular)
+from telegram_bot import TelegramBot
+telegram_bot = TelegramBot(chat_id = '65065237')
+telegram_bot.send_message('Optimization startded: '+str(normalized)+str(popular))
 run_recommender_optimization(normalized, popular)
-#telegram_bot.send_message('Optimization ended: '+str(normalized)+str(popular))
+telegram_bot.send_message('Optimization ended: '+str(normalized)+str(popular))
