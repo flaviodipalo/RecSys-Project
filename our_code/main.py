9a6b76217b3195f7d7511a7e2132b853806c1f53
alessio <alessio.baccell@mail.polimi.it>
2018-06-03 01:37:39 +0200

    Minor fixing

--- our_code/main.py
+++ our_code/main.py
@@ -37,7 +37,7 @@ else:
 def run_recommender(normalized, popular):
     #cython epoch only version
     print('Loading Data...')
-    data_reader = Movielens1MReader(train_test_split=0.6, train_validation_split=0.5, delete_popular=popular)
+    data_reader = Movielens1MReader(train_validation_split=[0.6, 0.2, 0.2], delete_popular=popular)
 
     URM_train = data_reader.URM_train
     URM_test = data_reader.URM_test
