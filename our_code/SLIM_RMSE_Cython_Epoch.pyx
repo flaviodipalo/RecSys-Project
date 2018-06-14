091f26a624f7edbdb83003130124fe9113954d18
alexbacce <alessio.baccelli@mail.polimi.it>
2018-06-14 20:45:27 +0200

    Minor changes

--- our_code/SLIM_RMSE_Cython_Epoch.pyx
+++ our_code/SLIM_RMSE_Cython_Epoch.pyx
@@ -155,7 +155,7 @@ cdef class SLIM_RMSE_Cython_Epoch:
         #GRADIENT DESCENT EPS FOR AVOIDING DIVISION BY ZERO
         self.eps = 10e-8
 
-        S = sp.sparse.random(self.n_movies, self.n_movies, format='csc', density=0.0000001)
+        S = sp.sparse.random(self.n_movies, self.n_movies, format='csc', density=0.01)
         self.S_indices = S.indices
         self.S_indptr = S.indptr
         self.S_data = S.data
