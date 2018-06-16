cb76b053f9f6872c268fb916b7845382a2b3519f
alexbacce <alessio.baccelli@mail.polimi.it>
2018-06-16 21:30:56 +0200

    Minor changes

--- our_code/main.py
+++ our_code/main.py
@@ -8,8 +8,8 @@ from data.book_crossing.BookCrossingReader import BookCrossingReader
 from data.movielens_10m.Movielens10MReader import Movielens10MReader
 import argparse
 
-#from ParameterTuning import BayesianSearch
-#from ParameterTuning.AbstractClassSearch import DictionaryKeys
+from ParameterTuning import BayesianSearch
+from ParameterTuning.AbstractClassSearch import DictionaryKeys
 
 #ssh -i /Users/flaviodipalo/Downloads/recsys-project.pem ubuntu@131.175.21.230
 parser = argparse.ArgumentParser()
