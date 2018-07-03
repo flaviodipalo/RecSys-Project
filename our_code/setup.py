c967dcca21600e4dbb50d515ba80a9d69eec01c9
alexbacce <alessio.baccelli@mail.polimi.it>
2018-07-03 11:51:44 +0200

    Minor Changes

--- our_code/setup.py
+++ our_code/setup.py
@@ -4,12 +4,6 @@ from Cython.Build import cythonize
 # python setup.py build_ext --inplace
 
 setup(
-  name='Hello world app',
-  ext_modules=cythonize("SLIM_RMSE_Cython_Epoch_Normalized.pyx"),
-  include_dirs=[numpy.get_include()]
-)
-
-setup(
   name='Hello',
   ext_modules=cythonize('SLIM_RMSE_Cython_Epoch_Normal.pyx'),
   include_dirs=[numpy.get_include()]
